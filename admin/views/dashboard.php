5e405217e548e52b3ded2540522aae781c878acd
Facundo <facundo.paez@davinci.edu.ar>
2024-07-12 21:22:46 -0300

    Mini fix

--- admin/views/dashboard.php
+++ admin/views/dashboard.php
@@ -7,7 +7,7 @@
                     <h3 class="mb-1">Administrar Productos</h3>
                     <p class="mb-1">Editar, borrar o subir nuevos productos.</p>
                 </a>
-                <a href="admin_usuarios.php" class="list-group-item list-group-item-action">
+                <a href="admin_usuarios.php" class="list-group-item list-group-item-action disabled" aria-disabled="true">
                     <h3 class="mb-1">Administrar Usuarios</h3>
                     <p class="mb-1">Ver la lista de usuarios y cambiar sus roles.</p>
                 </a>
