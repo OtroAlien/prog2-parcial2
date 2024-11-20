83ab1151d3aabb5ccd3ad7a65dccd2d3e62bc3db
Facundo <facundo.paez@davinci.edu.ar>
2024-11-19 23:31:16 -0300

    New Form

--- views/login.php
+++ views/login.php
@@ -1,32 +1,40 @@
 <div class="containerlogin">
-    <div class="loginform">  	
-        <input type="checkbox" id="chk" aria-hidden="true">
+    <div>
+        <?= (new Alerta())->get_alertas(); ?>
+    </div> 
+    <input type="checkbox" id="check">
+    <div class="login formu">
+        <header>Login</header>
+        <form action="admin/actions/auth_login.php" method="POST">
+            <input type="email" name="email" placeholder="Email" required="">
+            <input type="password" name="pswd" placeholder="Contraseña" required="">
+            <div class="boton">
+                <button class="button" type="submit">Ingresar</button>
+            </div>
+        </form>
         <div class="signup">
-            <div>
-                <?= (new Alerta())->get_alertas(); ?>
-            </div> 
-            <form action="admin/actions/auth_register.php" method="POST">
-                <label for="chk" aria-hidden="true">Registrate</label>
-                <input type="text" name="username" id="username" placeholder="Nombre de Usuario" required="">
-                <input type="text" name="nombre_completo" id="nombre_completo" placeholder="Nombre Completo" required="">
-                <input type="text" name="adress" id="adress" placeholder="Dirección" required="">
-                <input type="email" name="email" id="email" placeholder="Email" required="">
-                <input type="password" name="pswd" id="pswd" placeholder="Contraseña" required="">
-                <div class="boton">
-                    <button class="button" type="submit">Registrar</button>
-                </div>
-            </form>
+            <span class="signup">¿No tenés cuenta?
+                <label for="check">Registrate</label>
+            </span>
         </div>
+    </div>
 
-        <div class="login">
-            <form action="admin/actions/auth_login.php" method="POST">
-                <label for="chk" aria-hidden="true">Iniciar Sesión</label>
-                <input type="email" name="email" placeholder="Email" required="">
-                <input type="password" name="pswd" placeholder="Contraseña" required="">
-                <div class="boton">
-                    <button class="button" type="submit">Ingresar</button>
-                </div>
-            </form>
+    <div class="registration formu">
+        <header>Signup</header>
+        <form action="admin/actions/auth_register.php" method="POST">
+        <input type="text" name="username" id="username" placeholder="Nombre de Usuario" required="">
+        <input type="text" name="nombre_completo" id="nombre_completo" placeholder="Nombre Completo" required="">
+        <input type="text" name="adress" id="adress" placeholder="Dirección" required="">
+        <input type="email" name="email" id="email" placeholder="Email" required="">
+        <input type="password" name="pswd" id="pswd" placeholder="Contraseña" required="">
+        <div class="boton">
+            <button class="button" type="submit">Registrar</button>
+        </div>
+        </form>
+        <div class="signup">
+        <span class="signup">¿Ya tenés cuenta?
+            <label for="check">Inicia Sesión</label>
+        </span>
         </div>
     </div>
 </div>
